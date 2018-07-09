ebf1138684567767ba98466bffd989614e525559
Palash <palashw173@gmail.com>
2018-07-10 00:35:19 +0530

    reverse string problem done

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,8 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = (text) => {
+  const reversestr = text.split('').reverse().join('');
+  return reversestr;
+};
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,10 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (text) => {
+  let revstr = '';
+  for (let i = text.length - 1; i >= 0; i -= 1) {
+    revstr += text[i];
+  }
+  return revstr;
+};
 
 module.exports = reverse;
